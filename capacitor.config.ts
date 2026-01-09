0d62544d9b48efd0ceb1bc6e7af66dd6f38655dc
yolelukoie <137051592+yolelukoie@users.noreply.github.com>
2026-01-09 22:42:06 +0200

    new project config

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.familyhuddle.app',
+  appName: 'Family Huddle',
+  webDir: 'dist'
+};
+
+export default config;
